917de6d50cfb1392a97728fbe2b166c6f367d923
Spectre <EmailGax8@Gmail.com>
2019-09-29 21:45:39 +0400

    lol

---
+++ Assets/Scenes/Menu/Scripts/Testscript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Testscript : MonoBehaviour
+{
+    public const string test = "Пыка, надо невелировать последствия напыыыыыыыыыыыый";
+    void Start()
+    {
+        print(test); 
+    }
+
+    void Update()
+    {
+        
+    }
+}
