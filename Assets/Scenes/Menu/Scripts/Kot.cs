ce74a8c06f4c6c243d96c68645a9d79388d3478c
Spectre <EmailGax8@Gmail.com>
2019-10-06 16:22:17 +0400

    Kot

---
+++ Assets/Scenes/Menu/Scripts/Kot.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Kot : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //sdfgsdfgdfsg
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
