2b65cd661593b8cdeeac1705fc937de085e6285b
KoTeuKaSeeker <emaillit8@gmail.com>
2019-10-06 19:08:19 +0300

    Set black cubes

---
+++ Assets/Scenes/Menu/Scripts/CreateBlackCube.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateBlackCube : MonoBehaviour
+{
+    public GameObject BlackCube;
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Instantiate(BlackCube,transform.position, transform.rotation);
+        }
+    }
+}
