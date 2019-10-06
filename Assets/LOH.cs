801374599bd4cb38fc8831cb384df7b016588ae8
Spectre <EmailGax8@Gmail.com>
2019-10-06 17:28:21 +0400

    conflict naher!

--- Assets/LOH.cs
+++ Assets/LOH.cs
@@ -4,13 +4,11 @@ using UnityEngine;
 
 public class LOH : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float loh = 1;
     void Start()
     {
-        
+        loh = 5;
     }
-
-    // Update is called once per frame
     void Update()
     {
         
