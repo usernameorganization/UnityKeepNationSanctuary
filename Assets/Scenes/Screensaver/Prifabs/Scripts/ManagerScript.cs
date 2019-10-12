1e21ba7b1a4332147fabd917650fb1b0c3a95f85
KoTeuKaSeeker <emaillit8@gmail.com>
2019-10-12 18:40:50 +0300

    ScreenSave

---
+++ Assets/Scenes/Screensaver/Prifabs/Scripts/ManagerScript.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ManagerScript : MonoBehaviour
+{
+    public GameObject[] layer;
+    public int delay;
+
+    private int id;
+    private bool isCheck;
+    private int timer;
+
+    private int delayTimer;
+
+    private Animator backgroundImage;
+
+    private GameObject backGround;
+    private GameObject showingImage;
+
+    public bool isAnimationPlaying(Animator animator, string nameAnimation)
+    {
+        AnimatorStateInfo state = animator.GetCurrentAnimatorStateInfo(0);
+        return state.IsName(nameAnimation);
+
+    }
+
+    public void setObjects(int id)
+    {
+        showingImage = layer[id].transform.Find("1").gameObject;
+        backGround = layer[id].transform.Find("2").gameObject;
+        backgroundImage = backGround.GetComponent<Animator>();
+    }
+
+    public void activeAllAnimations()
+    {
+        setObjects(id);
+        bool isActive = isAnimationPlaying(backgroundImage, "ShowTheCanvas");
+        if (!isActive)
+        {
+            if (delayTimer <= 0)
+            {
+                layer[id].SetActive(false);
+                id++;
+                delayTimer = delay;
+                if (id >= layer.Length)
+                {
+                    isCheck = false;
+                }
+                else
+                {
+                    setObjects(id);
+                    backgroundImage.Play("ShowTheCanvas", 0);
+                }
+            }
+            else
+            {
+                delayTimer--;
+            }
+        }
+    }
+
+    void Start()
+    {
+        timer = 10;
+        delayTimer = delay;
+
+        setObjects(0);
+
+        backgroundImage.Play("ShowTheCanvas", 0);
+        isCheck = true;
+    }
+
+    void Update()
+    {
+        if (timer > 0) timer--;
+        else
+        {
+            if (isCheck)
+            {
+                if (delayTimer <= 0) delayTimer = 0;
+                activeAllAnimations();
+            }
+        }
+    }
+}
