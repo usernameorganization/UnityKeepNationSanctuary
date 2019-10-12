1871383f67f26fd662f006b106583ee2234624c3
KoTeuKaSeeker <emaillit8@gmail.com>
2019-10-12 13:29:24 +0300

    SettingsMenu

---
+++ Assets/Scripts/OpenMenu.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OpenMenu : MonoBehaviour
+{
+
+    public Button button;
+
+    private bool isOpen;
+    public Animator animator;
+
+    public void Start()
+    {
+        button.onClick.AddListener(buttonMethod);
+        isOpen = true;
+    }
+
+    public void Update()
+    {
+        
+    }
+
+    public void buttonMethod()
+    {
+        if (isOpen)
+        {
+            animator.SetBool("isOpen", isOpen);
+            isOpen = false;
+        }
+        else
+        {
+            animator.SetBool("isOpen", isOpen);
+            isOpen = true;
+        }
+    }
+}
